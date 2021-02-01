96128aa291bed00f4cca2482484dedddc044f4e6
taima99sh <taima99sh@hotmail.com>
2021-02-01 10:22:33 +0200

    Initial Commit

---
+++ Istsharat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Istsharat
+//
+//  Created by taima on 2/1/21.
+//  Copyright © 2021 mac air. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
